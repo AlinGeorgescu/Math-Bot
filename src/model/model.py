b02621bd73640da756ae77c12c0d3d2ccf1e32b8
AlinGeorgescu <alinandrei2007@yahoo.com>
2021-05-05 16:12:56 +0300

    Coding style improvements

--- src/model/model.py
+++ src/model/model.py
@@ -8,46 +8,45 @@ Computer Engeneering Department
 
 Math Bot (C) 2021 - The agent's AI model
 
-This code is part of "model/SiameseLSTM.ipynb"
+This code is part of "model/SiameseLSTM.ipynb". Some function and variable names
+are changed with regard to the coding style conventions.
 """
 
 import json
+import random as rnd
 import re
 
+from collections import defaultdict
+
 import numpy as np
-import random as rnd
 import textcleaner as tc
 
 from trax import layers as tl
 from trax.fastmath import numpy as fastnp
 
-from collections import defaultdict
-
-def Siamese(vocab_size, d_model=128, mode="train"):
+def siamese(vocab_size, d_model=128):
     """Returns a Siamese model.
 
     Args:
         vocab_size (int, optional): Length of the vocabulary. Defaults to
                                     len(vocab).
         d_model (int, optional): Depth of the model. Defaults to 128.
-        mode (str, optional): "train", "eval" or "predict", predict mode is for
-                              fast inference. Defaults to "train".
 
     Returns:
         trax.layers.combinators.Parallel: A Siamese model.
     """
 
-    def normalize(x):  # normalizes the vectors to have L2 norm 1
-        return x / fastnp.sqrt(fastnp.sum(x * x, axis=-1, keepdims=True))
+    def normalize(vec):  # normalizes the vectors to have L2 norm 1
+        return vec / fastnp.sqrt(fastnp.sum(vec * vec, axis=-1, keepdims=True))
 
-    s_processor = tl.Serial(                        # Will run on S1 and S2
-        tl.Embedding(vocab_size, d_model),          # Embedding layer
-        tl.LSTM(d_model, mode=mode),                # LSTM layer
-        tl.Mean(axis=1),                            # Mean over columns
-        tl.Fn('Normalize', lambda x: normalize(x))  # Apply normalize function
+    s_processor = tl.Serial(
+        tl.Embedding(vocab_size, d_model),  # Embedding layer
+        tl.LSTM(d_model),                   # LSTM layer
+        tl.Mean(axis=1),                    # Mean over columns
+        tl.Fn('Normalize', normalize)       # Apply normalize function
     )  # Returns one vector of shape [batch_size, d_model].
 
-    # Run on S1 and S2 in parallel.
+    # Run on s1_tensor and s2_tensor in parallel.
     model = tl.Parallel(s_processor, s_processor)
     return model
 
@@ -63,7 +62,7 @@ def data_loader():
         vocab_dict = json.load(fin)
 
     vocab = defaultdict(lambda: 0, vocab_dict)
-    model = Siamese(vocab_size=len(vocab))
+    model = siamese(vocab_size=len(vocab))
     model.init_from_file("trax_model/model.pkl.gz")
 
     return (vocab, model)
@@ -107,12 +106,12 @@ def data_tokenizer(sentence):
 
     return tc.token_it(tc.lemming(tc.stemming(sentence)))[0]
 
-def data_generator(S1, S2, batch_size, pad=1, shuffle=False, verbose=False):
+def data_gen(sentences1, sentences2, batch_size, pad=1, shuffle=False):
     """Generator function that yields batches of data
 
     Args:
-        S1 (list): List of transformed (to tensor) sentences.
-        S2 (list): List of transformed (to tensor) sentences.
+        sentences1 (list): List of transformed (to tensor) sentences.
+        sentences2 (list): List of transformed (to tensor) sentences.
         batch_size (int): Number of elements per batch.
         pad (int, optional): Pad character from the vocab. Defaults to 1.
         shuffle (bool, optional): If the batches should be randomnized or not.
@@ -131,7 +130,7 @@ def data_generator(S1, S2, batch_size, pad=1, shuffle=False, verbose=False):
     input1 = []
     input2 = []
     idx = 0
-    len_s = len(S1)
+    len_s = len(sentences1)
     sentence_indexes = [*range(len_s)]
 
     if shuffle:
@@ -145,13 +144,13 @@ def data_generator(S1, S2, batch_size, pad=1, shuffle=False, verbose=False):
             if shuffle:
                 rnd.shuffle(sentence_indexes)
 
-        s1 = S1[sentence_indexes[idx]]
-        s2 = S2[sentence_indexes[idx]]
+        s1_tensor = sentences1[sentence_indexes[idx]]
+        s2_tensor = sentences2[sentence_indexes[idx]]
 
         idx += 1
 
-        input1.append(s1)
-        input2.append(s2)
+        input1.append(s1_tensor)
+        input2.append(s2_tensor)
 
         if len(input1) == batch_size:
             # Determine max_len as the longest sentence in input1 & input 2
@@ -160,67 +159,55 @@ def data_generator(S1, S2, batch_size, pad=1, shuffle=False, verbose=False):
             # Pad to power-of-2
             max_len = 2 ** int(np.ceil(np.log2(max_len)))
 
-            b1 = []
-            b2 = []
-            for s1, s2 in zip(input1, input2):
-                # Add [pad] to s1 until it reaches max_len
-                s1 = s1 + [pad] * (max_len - len(s1))
-                # Add [pad] to s2 until it reaches max_len
-                s2 = s2 + [pad] * (max_len - len(s2))
+            tmp_out1 = []
+            tmp_out2 = []
+            for s1_tensor, s2_tensor in zip(input1, input2):
+                # Add [pad] to s1_tensor until it reaches max_len
+                s1_tensor = s1_tensor + [pad] * (max_len - len(s1_tensor))
+                # Add [pad] to s2_tensor until it reaches max_len
+                s2_tensor = s2_tensor + [pad] * (max_len - len(s2_tensor))
 
-                # Append s1
-                b1.append(s1)
-                # Append s2
-                b2.append(s2)
+                # Append s1_tensor
+                tmp_out1.append(s1_tensor)
+                # Append s2_tensor
+                tmp_out2.append(s2_tensor)
 
-            if verbose == True:
-                print("input1 = ", input1, "\ninput2 = ", input2)
-                print("b1     = ", b1, "\nb2     = ", b2)
-
-            # Use b1 and b2
-            yield np.array(b1), np.array(b2)
+            # Use tmp_out1 and tmp_out2
+            yield np.array(tmp_out1), np.array(tmp_out2)
 
             # reset the batches
             input1, input2 = [], []
 
-def predict(sentence1, sentence2, threshold, model, vocab,
-            data_generator=data_generator, verbose=False):
+def predict(sentences, threshold, model, vocab, data_generator=data_gen):
     """Function for predicting if two sentences are duplicates.
 
     Args:
-        sentence1 (str): First sentence.
-        sentence2 (str): Second sentence.
+        sentences ((str, str)): The tuple containing the sentences.
         threshold (float): Desired threshold.
         model (trax.layers.combinators.Parallel): The Siamese model.
         vocab (collections.defaultdict): The vocabulary used.
-        data_generator (function): Data generator function. Defaults to
-                                   data_generator.
-        verbose (bool, optional): If the results should be printed out.
-                                  Defaults to False.
+        data_gen (function): Data generator function. Defaults to data_gen.
 
     Returns:
         bool: True if the sentences are duplicates, False otherwise.
     """
 
-    s1 = data_tokenizer(sentence1)  # tokenize
-    s2 = data_tokenizer(sentence2)  # tokenize
-    S1, S2 = [], []
+    s1_tokens = data_tokenizer(sentences[0])  # tokenize
+    s2_tokens = data_tokenizer(sentences[1])  # tokenize
 
-    for word in s1:  # encode s1
-        S1 += [vocab[word]]
-    for word in s2:  # encode s2
-        S2 += [vocab[word]]
+    s1_tensor, s2_tensor = [], []
 
-    S1, S2 = next(data_generator([S1], [S2], 1, vocab["<PAD>"], False, verbose))
+    for word in s1_tokens:  # encode sentence1
+        s1_tensor += [vocab[word]]
+    for word in s2_tokens:  # encode sentence2
+        s2_tensor += [vocab[word]]
 
-    v1, v2 = model((S1, S2))
-    d = np.dot(v1[0], v2[0].T)
-    res = d > threshold
+    s1_tensor, s2_tensor = \
+            next(data_generator([s1_tensor], [s2_tensor], 1, vocab["<PAD>"]))
 
-    if verbose == True:
-        print("S1  = ", S1, "\nS2  = ", S2)
-        print("d   = ", d)
-        print("res = ", res)
+    vec1, vec2 = model((s1_tensor, s2_tensor))
+    sim = np.dot(vec1[0], vec2[0].T)
+    res = sim > threshold
 
     return res
 
